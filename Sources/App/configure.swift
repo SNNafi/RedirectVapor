380d52a86e7e01cb18d4fa442a5445800cb2a63b
SNNAFI <shahriarnasim.nafi@gmail.com>
2021-01-27 07:55:15 +0600

    Build for Server Testing with postgres

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,6 +1,6 @@
 import Vapor
 import Fluent
-import FluentMySQLDriver
+import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
@@ -8,13 +8,13 @@ public func configure(_ app: Application) throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
     app.http.server.configuration.port = 8081
-    app.databases.use(.mysql(
+    app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        username: Environment.get("DATABASE_USERNAME") ?? "redirect",
-        password: Environment.get("DATABASE_PASSWORD")  ?? "redirect",
-        database: Environment.get("DATABASE_NAME") ?? "redirect",
-        tlsConfiguration: .forClient(certificateVerification: .none)
-      ), as: .mysql)
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "tilapp",
+        password: Environment.get("DATABASE_PASSWORD") ?? "tilapp",
+        database: Environment.get("DATABASE_NAME") ?? "tilapp"
+    ), as: .psql)
 
 
     app.migrations.add(CreateRedirect())
